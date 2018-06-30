6d47dcf1917f898d45f7cf9e86c8d8ca4a5c76db
Vladislav Zhadchenko <zhadchenko.vlad@outlook.com>
2018-06-30 22:37:42 +0300

    Setting up the project(Some modules have been added)

--- internet-shop/src/index.js
+++ internet-shop/src/index.js
@@ -1,8 +1,29 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import thunk from 'redux-thunk'
+import {composeWithDevTools} from 'redux-devtools-extension'
+import {createStore, applyMiddleware} from 'redux'
 import './index.css';
-import App from './App';
-import registerServiceWorker from './registerServiceWorker';
+import {browserHistory} from 'react-router'
+import reducers from './reducers/index'
 
-ReactDOM.render(<App />, document.getElementById('root'));
-registerServiceWorker();
+import {syncHistoryWithStore} from 'react-router-redux'
+import {Provider} from 'react-redux'
+import {Router, Route} from 'react-router'
+import Layout from './containers/layout'
+import Phones from './containers/phones'
+
+
+const store = createStore(reducers, composeWithDevTools(applyMiddleware(thunk)));
+const history = syncHistoryWithStore(browserHistory, store);
+
+ReactDOM.render(<Provider store={store}>
+        <Router history={history}>
+            <Route component={Layout}>
+                <Route path='/' component={Phones} />
+            </Route>
+
+        </Router>
+    </Provider>
+  ,document.getElementById('root')
+);
