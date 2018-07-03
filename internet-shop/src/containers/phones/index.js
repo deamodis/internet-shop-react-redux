6b4b7a38b27f94d8f4ba62da7941f38092ab7b00
deamodis <zhadchenko.vladislav@gmail.com>
2018-07-03 19:18:10 +0300

    Display phones added

--- internet-shop/src/containers/phones/index.js
+++ internet-shop/src/containers/phones/index.js
@@ -1,18 +1,56 @@
 import React, {Component} from 'react'
 import {connect} from 'react-redux'
-
+import {Link} from 'react-router'
 import {fetchPhones} from '../../actions/index'
-import {getPhones} from 'selectors'
+import {getPhones} from '../../selectors'
+import * as R from 'ramda'
 
 class Phones extends Component{
     componentDidMount() {
      this.props.fetchPhones();
     }
 
+
+    renderPhone (phone, index){
+        const shortDescription = R.take(60, phone.description)
+
+        return <div className="col-sm-4 col-lg-4 col-md-4 book-list" key={index}>
+            <div className="thumbnail">
+                <img className='mg-thumbnail'
+                     src={phone.image}
+                     alt={phone.name}
+                />
+                <div className="caption">
+                    <h4 className="pull-right">${phone.price}</h4>
+                    <h4>
+                        <Link to={`/phones/${phone.id}`}>
+                            {phone.name}
+                        </Link>
+                    </h4>
+                    <p>{shortDescription}</p>
+                    <p className="itemButton">
+                        <button className='btn btn-primary'>
+                            Buy now
+                        </button>
+                        <Link to={`/phones/${phone.id}`}
+                              className="btn btn-default"
+                        >
+                            More info
+                        </Link>
+                    </p>
+                </div>
+            </div>
+        </div>
+    }
+
     render(){
+        const {phones} = this.props;
+
         return(
             <div>
-                Phones
+                <div className="books row">
+                    {phones.map((phone, index)=> this.renderPhone(phone, index))}
+                </div>
             </div>
         )
     }
@@ -26,5 +64,5 @@ const mapDispatchToProps = {
     fetchPhones
 };
 
-export default connect(null, mapDispatchToProps)(Phones) // в connect берутся методы mapDispatchToProps и вызываются,
+export default connect(mapStateToProps, mapDispatchToProps)(Phones) // в connect берутся методы mapDispatchToProps и вызываются,
 // в отличии от того, когда мы передаём функцию dispatch => ... как аргумент
--- internet-shop/src/selectors.js
+++ internet-shop/src/selectors.js
@@ -1,4 +1,4 @@
-import R from 'ramda'
+import * as R from 'ramda'
 
 const getPhoneById = (state, id) => R.prop(id, state.phones)
 
